bfff18c765655adee409e2aca043f63ea0a419c5
Alexander <ksndr@icloud.com>
2019-04-28 07:33:06 +0300

    hw4

--- Lesson4/hw4-2.py
+++ Lesson4/hw4-2.py
@@ -5,19 +5,48 @@
 
 import timeit
 
+# нахождение простых числе через перебор делителей
 
-def natur(ff: int):
+
+def natur(f):
     s = []
-    for i in range(1, ff + 1):
+    for i in range(2, f + 1):
         for j in range(2, i):
             if i % j == 0:
                 break
         else:
             s.append(i)
+    return s
 
 
-a = int(input())
-natur(a)
+# нахождение через решето
+def erat(f):
+    s = []
+    e = [int(i) for i in range(2, f + 1)]
+    num = 2  # установка первого простого числа
+    n = 2
+    for i in range(1, len(e)):  # запуск перебора чисел
+        while n <= f:  # запуск цикла перебора чисел кратных простому
+            for j in range(i+1, len(e)):  # проверка на кратность
+                if e[j] == n:
+                    e[j] = 0  # "прокол" числа, кратного взятому простому числу
+                    break  # остановка прогона, после нахождения
+                elif e[j] > n:
+                    break  # остановка прогона, после прохождения установленного числа для проверки кратности
+            n += num  # установка следующего кратного числа
+        if e[i] > num:  # поиск оставшегося в решете числа, после цикла удаления кратных чисел по предудущему простому числу
+            num = e[i]
+            n = num
+    for i in e:  # создание списка простых чисел в заданном диапозоне
+        if i > 0:
+            s.append(i)
+    return s
 
-print(timeit.timeit("natur(ff)", setup="from __main__ import natur", number=1, globals={"ff": ff}))
+
+f = int(input())
+print(natur(f))
+print(timeit.timeit("natur(f)", setup="from __main__ import natur", number=1, globals={"f": f}))
+print(erat(f))
+print(timeit.timeit("erat(f)", setup="from __main__ import erat", number=1, globals={"f": f}))
+
 
