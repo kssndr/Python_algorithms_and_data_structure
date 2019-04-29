5bf5977e43f0ba1feb4c0fdf9e276de2d8b40d99
Alexander <ksndr@icloud.com>
2019-04-29 17:43:38 +0300

    hw5

--- Lesson5/hw5-2.py
+++ Lesson5/hw5-2.py
@@ -4,5 +4,89 @@
 # примера: [‘C’, ‘F’, ‘1’], произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 #
 # Примечание: для решения задач попробуйте применить какую-нибудь коллекцию из модуля collections (пусть это и не
-# очевидно с первого раза. Вы же не Голландец ;-).
+# # очевидно с первого раза. Вы же не Голландец ;-).
+# Алгоритм:
+# 1) принимаем вводимые числа как строки
+# 2) сохраняем их как массивы
+# 3) реверсируем их для сложения
+# 4) складываем через цикл, занося сумму в новый массив
+# 5) реверсируем массив с суммой обратно и выводим через цикл
+# 6) умножаем через цикл, занося произведение в новый массив
+# 7) выводим результат
+#
 
+import collections
+
+
+def abs123(q):
+    l = len(q)
+    for i in range(l):
+        if q[i] == 'a':
+            q[i] = 10
+        elif q[i] == 'b':
+            q[i] = 11
+        elif q[i] == 'c':
+            q[i] = 12
+        elif q[i] == 'd':
+            q[i] = 13
+        elif q[i] == 'e':
+            q[i] = 14
+        elif q[i] == 'f':
+            q[i] = 15
+    return q
+
+def to16(s):
+    l = len(s)
+    for i in range(l):
+        if s[i] == 10:
+            s[i] = 'a'
+        elif s[i] == 11:
+            s[i] = 'b'
+        elif s[i] == 12:
+            s[i] = 'c'
+        elif s[i] == 13:
+            s[i] = 'd'
+        elif s[i] == 14:
+            s[i] = 'e'
+        elif s[i] == 15:
+            s[i] = 'f'
+    return s
+
+def sum16(x, y):
+    z = []
+    xx = abs123(x[::-1])
+    yy = abs123(y[::-1])
+    if len(xx) >= len(yy):
+        for i in range(len(xx) - len(yy)):
+            yy.append(0)
+    else:
+        for i in range(len(yy) - len(xx)):
+            xx.append(0)
+    l = len(xx)
+    p, zz, zzz = 0, 0, 0
+    for i in range(l):
+        zz = int(xx[i]) + int(yy[i]) + p
+        if zz >= 16:
+            p = zz // 16
+            zzz = zz % 16
+        else:
+            zzz = zz
+            p = 0
+        z.append(zzz)
+
+    return to16(z[::-1])
+
+
+a = input("введите первое число в 16 формате: ")
+b = input("введите второе число в 16 формате: ")
+# a = 'fa12c'
+# b = '39bd'
+aa = list(a)
+bb = list(b)
+
+sum = sum16(aa, bb)
+
+print(f"Сумма чисел {a} и {b}:")
+for i in sum:
+    print(i, end="")
+    
