8720df191f2e713028bb9375c28c47cc29ff6d57
Alexander <ksndr@icloud.com>
2019-05-17 11:31:59 +0300

    hw8_2

--- Lesson8/hw8_2.py
+++ Lesson8/hw8_2.py
@@ -9,18 +9,19 @@ import random
 from binarytree import Node
 from binarytree import build
 
-v = int(input("Сгенерировать строку автоматически или ввести вручную? (1/2) "))
+# v = int(input("Сгенерировать строку автоматически или ввести вручную? (1/2) "))
+#
+# if v == 1:
+#     n = int(input("Введите размер строки: "))
+#     source_string = ("".join([chr(random.randint(48, 52)) for i in range(n)]))
+# elif v == 2:
+#     source_string = (input("Введите строку: "))
+#     n = len(source_string)
+#
+# print("*" * n, "\n", source_string, "\n", "*" * n)
 
-if v == 1:
-    n = int(input("Введите размер строки: "))
-    source_string = ("".join([chr(random.randint(48, 52)) for i in range(n)]))
-elif v == 2:
-    source_string = (input("Введите строку: "))
-    n = len(source_string)
-
-print("*" * n, "\n", source_string, "\n", "*" * n)
-
-ss = source_string
+# ss = source_string
+ss = "apple"
 
 # 2. создаем словарь с данными по буква: частота
 l_q = dict()
@@ -29,18 +30,32 @@ for i in range(len(ss)):
     for j in range(len(ss)):
         if ss[i] is not l_q and ss[i] == ss[j]:
             q += 1
-    l_q[ss[i]] = q
+    l_q[ss[i]] = q/len(ss)
 print(l_q)
 l_q_s = dict(sorted(l_q.items(), key=lambda x: x[1]))
 
 # 3. создаем дерево по алгоритму Хаффмана
-
-
+'''
 print("sorted dict", l_q_s)
 k = l_q_s.keys()
 v = l_q_s.values()
+print("длина: ", len(l_q_s))
+node = {}
+ii = 1
+for i in l_q_s.items():
+    node[ii] = i
+    ii += 1
+print("node", node)
+
+
 print("k", k)
 print("v", v)
-root = build(v)
+root = build(node.keys())
 print("root", root)
 print("leaves", root.leaves)
+print("root and index", root.pprint(index=True))
+
+'''
+
+class Node:
+
--- Lesson8/tree.py
+++ Lesson8/tree.py
@@ -1,7 +1,11 @@
 class Node:
-    def __init__(self, value=None, left=None, right=None):
+    def __init__(self, value=None, left=None, right=None, symbol=None, frequency=None):
         self.value = value
         self.left = left
         self.right = right
+        self.symbol = symbol
+        self.frequency = frequency
+
+
 
 
