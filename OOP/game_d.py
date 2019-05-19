a441622681d56a45df5a20eaf617ca26310e59bf
Alexander <ksndr@icloud.com>
2019-05-19 15:11:40 +0300

    OOP ex over

--- OOP/game_d.py
+++ OOP/game_d.py
@@ -54,12 +54,12 @@ class Desk:
         if len(self.cards) == 0:
             return None
         else:
-            self.cards.pop()
+            return self.cards.pop()
 
 
-desk = Desk()
-for card in desk.cards:
-    print(card)
+# desk = Desk()
+# for card in desk.cards:
+#     print(card)
 
 
 class Player:
@@ -67,4 +67,62 @@ class Player:
         self.name = name
         self.wins = 0
         self.cards = None
+
+
+# alex = Player("Alexander")
+# print(alex.name)
+
+class Game:
+    def __init__(self):
+        name1 = input("Player 1: ")
+        name2 = input("Player 2: ")
+        self.desk = Desk()
+        self.p1 = Player(name1)
+        self.p2 = Player(name2)
+
+    def wins(self, winner):
+        w = "{} take the cards"
+        w = w.format(winner)
+        print(w)
+
+    def draw(self, p1n, p1c, p2n, p2c):
+        d = "{} puts {} and {} puts {}"
+        d = d.format(p1n, p1c, p2n, p2c)
+        print(d)
+
+    def play_game(self):
+        cards = self.desk.cards
+        print("Play!")
+        while len(cards) > 2:
+            m = "Play game or press X to exit"
+            responce = input(m)
+            if responce == "X":
+                break
+            p1c = self.desk.rm_card()
+            p2c = self.desk.rm_card()
+            p1n = self.p1.name
+            p2n = self.p2.name
+            self.draw(p1n, p1c, p2n, p2c)
+            if p1c > p2c:
+                self.p1.wins += 1
+                self.wins(self.p1.name)
+            else:
+                self.p2.wins += 1
+                self.wins(self.p2.name)
+
+        win = self.winner(self.p1, self.p2)
+
+        print("Game over. {} winner!".format(win))
+
+    def winner(self, p1, p2):
+        if p1.wins > p2.wins:
+            return p1.name
+        if p2.wins > p1.wins:
+            return p2.name
+        return "No winner"
+
+
+game = Game()
+game.play_game()
+
 
